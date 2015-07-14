05cc0f3bc9029b98ba4eae2e3c93d2487d1aaa5a
Mark Harris <harrisdm@hotmail.com>
2015-07-15 04:44:29 +1000

    about to merge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,13 +12,22 @@ module ApplicationHelper
 
   
   def link_to_add_fields(name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
+    new_object = f.object.class.reflect_on_association(association).klass.create
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
     id = new_object.object_id
     link_to(name, '#', class: "add_link", data: {class: id, fields: fields.gsub("\n", "")})
   end
+
+  #   def link_to_add_fields(name, f, association)
+  #   new_object = f.object.class.reflect_on_association(association).klass.new
+  #   fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+  #     render(association.to_s.singularize + "_fields", :f => builder)
+  #   end
+  #   id = new_object.object_id
+  #   link_to(name, '#', class: "add_link", data: {class: id, fields: fields.gsub("\n", "")})
+  # end
 
 
 end
