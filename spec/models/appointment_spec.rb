e682e785690998d709b5d99784b0550b8cc5afa6
Toby Flemming <tobyf93@hotmail.com>
2015-07-14 15:01:21 +1000

    Annotate

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: appointments
+#
+#  id                   :integer          not null, primary key
+#  appointment_block_id :integer
+#  participant_id       :integer
+#  datetime             :datetime
+#  phone                :string
+#  email                :string
+#  reminder             :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
