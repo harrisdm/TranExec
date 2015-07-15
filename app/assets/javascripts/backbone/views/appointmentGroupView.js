0aad778bca9ca5cb84b8ce77bc40fb8d5eef4bae
Toby Flemming <tobyf93@hotmail.com>
2015-07-15 18:00:22 +1000

    Display nice dates for appointmentGroup

--- app/assets/javascripts/backbone/views/appointmentGroupView.js
+++ app/assets/javascripts/backbone/views/appointmentGroupView.js
@@ -8,6 +8,13 @@ app.Views.AppointmentGroupView = Backbone.View.extend({
     var viewTemplate = $('#appointmentGroupTemplate').html();
     var viewHTML = _.template(viewTemplate);
 
-    parentView.append(viewHTML());
+    var groupHeader = this.groupHeader();
+    parentView.append(viewHTML({ groupHeader: groupHeader }));
+  },
+
+  groupHeader: function() {
+    var datetime = this.collection[0].get('datetime');
+    var day = moment(datetime);
+    return day.format('dddd Do MMMM');
   }
 });
