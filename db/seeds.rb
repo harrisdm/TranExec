7b558c6cef5e0e3f468af668b20f5afc9801bf3a
Mark Harris <harrisdm@hotmail.com>
2015-07-17 09:35:18 +1000

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,14 @@ wst1 = WorkshopType.create :name => 'Workshop 1'
 wst2 = WorkshopType.create :name => 'Workshop 2'
 wst3 = WorkshopType.create :name => 'Workshop 3'
 
-u1 = User.create :name => "Rich", :coach => false
+u1 = User.create :name => "Rich Lambert", :coach => false
 u2 = User.create :name => "Karen", :coach => true
 u3 = User.create :name => "Sarit", :coach => true
+u4 = User.create :name => "John", :coach => true
+u5 = User.create :name => "George", :coach => true
+u6 = User.create :name => "Charity", :coach => true
+u7 = User.create :name => "Ellie", :coach => true
+u8 = User.create :name => "Michaela", :coach => true
+u9 = User.create :name => "Rosmary", :coach => true
 
 
